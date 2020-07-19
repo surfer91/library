e56fbfe0bc1b9fd32a9a754103d9a5af07e3c3f1
Krzysztof Klimczak <l2eu@wp.pl>
2020-07-19 17:40:56 +0200

    Eslint, nodemon

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const express=require('express');
-const debug=require('debug')('app');
-const morgan=require('morgan');
-const path=require('path')
+const express = require('express');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-const app=express();
+const app = express();
+const port=process.env.PORT||3000;
 
 app.use(morgan('tiny'));
 
-app.use(express.static(path.join(__dirname,'public')));
-app.use('/css', express.static(path.join(__dirname,'node_module/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname,'node_module/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname,'node_module/jquery/dist')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, 'node_module/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_module/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, 'node_module/jquery/dist')));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 
-app.get('/',function(req,res){
-res.sendFile(path.join(__dirname,'views/index.html'));
-})
-
-app.listen(3000, function(){
-    debug('listenig on port 3000');
+app.listen(port, () => {
+  debug(`listenig on port ${port}`);
 });
