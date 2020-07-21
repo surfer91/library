5839890f56c2be99dcd30f3b3a3386c8e15734a3
Krzysztof Klimczak <l2eu@wp.pl>
2020-07-21 17:28:12 +0200

    Add routing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 const port=process.env.PORT||3000;
-
+const bookRouter=express.Router();
 app.use(morgan('tiny'));
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,8 +15,16 @@ app.use('/js', express.static(path.join(__dirname, 'node_module/jquery/dist')));
 app.set('views','./src/views');
 app.set('view engine','ejs');
 
+bookRouter.route('/').get((req, res) => {
+  res.send('hello books');})
+
+  
+bookRouter.route('/single').get((req, res) => {
+  res.send('hello single book');})
+
+  app.use('/books',bookRouter);
 app.get('/', (req, res) => {
-  res.render('index',{list:['a','b'],title:'Library'});
+  res.render('index',{nav:[{link:'/books',title:'Books'},{link:'/authors',title:'Author'}],title:'Library'});
 });
 
 app.listen(port, () => {
